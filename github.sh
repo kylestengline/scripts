f08894680ea8cf144bd5bfd9d9d6b2c1ee97c398
Kyle Stengline <kyle.stengline@gmail.com>
2016-07-09 13:48:01 -0400

    adds github script

---
+++ github.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+if [ -z "$1" ] || [ -z "$2"  ]
+then
+  echo "This script takes two arguments: Your github username and the name of your repo. 
+  EX: github <githubusername> <reponame>"
+  exit 1
+else
+  echo "Creating Git Repo named: "$2""
+  mkdir $2
+  cd $2
+                  
+  echo "Creating README and .gitignore for "$2"" 
+  touch README.md
+  touch .gitignore
+  echo "# $2" >> README.md
+
+  echo "Initializing and adding Git Repo"
+  git init
+  git add .
+
+  echo "Making Initial Commit"
+  git commit -m "Initial Commit"
+
+  echo "Adding remote origin"
+  git remote add origin git@github.com:$1/$2.git
+
+  echo "Pushing to master"
+  git push origin master
+  echo "Repo Created! YEA!"
+  cd $2
+  tree
+fi
