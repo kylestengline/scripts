668f02ccf0a4ede42cd524a4f34699b92c3cdaf2
Kyle Stengline <kyle.stengline@gmail.com>
2016-08-31 18:17:21 -0400

    refactors bring_me script

---
+++ bring_me.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#Opens My Udemy courses and netflix
+
+case "$1" in                                                                                                                             
+  "react")
+    open https://www.udemy.com/react-redux/learn/v4/overview
+    ;;
+  "ios")
+    open https://www.udemy.com/ios9-swift/learn/v4/overview
+    ;;
+  "js")
+    open https://www.udemy.com/understand-javascript/learn/v4/overview
+    ;;
+  "rspec")
+    open https://www.udemy.com/rubyonrails-bdd-rspec-capybara/learn/v4/overview
+    ;;
+  "scripting")
+    open https://www.udemy.com/shell-scripting-linux/learn/v4/overview
+    ;;
+  "netflix")
+    open https://www.netflix.com/browse
+    ;;
+  *)
+    echo "This script takes one of the following arguments: react, ios, js, rspec, scripting, netflix" ; exit 1
+    ;;
+esac
