30977e15c53bb245ea94447606d58a5cb75de7b1
Kyle Stengline <kyle.stengline@gmail.com>
2017-02-16 15:49:00 -0800

    updates bring me script

--- bring_me.sh
+++ bring_me.sh
@@ -4,15 +4,21 @@
 case "$1" in                                                                                                                             
   "react")
     open https://www.udemy.com/react-redux/learn/v4/overview
+    open https://www.icloud.com/pages/
+    cd ~/code/wdi/react-books
     ;;
   "ios")
     open https://www.udemy.com/ios9-swift/learn/v4/overview
+    open https://www.icloud.com/pages/
     ;;
   "js")
     open https://www.udemy.com/understand-javascript/learn/v4/overview
+    open https://www.icloud.com/pages/
     ;;
   "rspec")
     open https://www.udemy.com/rubyonrails-bdd-rspec-capybara/learn/v4/overview
+    open https://www.icloud.com/pages/
+    cd ~/code/wdi/blog_app_tests
     ;;
   "netflix")
     open https://www.netflix.com/browse
